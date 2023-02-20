224db55448a90c997b06ba0803d2e54f25be2a3f
Eng Happi Gilbert <gdushimimana6@gmail.com>
2023-02-20 02:29:39 -0800

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,15 @@
 *
 Return always 0 (Success)
 */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+int main(void)
+{
+	puts(""Programming is like building a multilingual puzzle\n");
+//	printf("with proper grammar, but the outcome is a piece of art,\n")
+//	printf("Size of a char: %d byte(s)\n",sizeof(char));
+//	printf("Size of an int: %d byte(s)\n",sizeof(int));
+//	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
+//	printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
+//	printf("Size of a float: %d byte(s)\n",sizeof(float));
+//write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59)
+	return (0);
 }
