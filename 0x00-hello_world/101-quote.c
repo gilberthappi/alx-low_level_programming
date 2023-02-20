db2077f925fdc27c30ed94ae829791ca2c6c79c3
Eng Happi Gilbert <gdushimimana6@gmail.com>
2023-02-20 03:58:53 -0800

    aha

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
 #include<stdio.h>
+#include <unistd.h>
+
 /**
-*main ~ Entry point
-*
-Return always 0 (Success)
-*/
+ * main - Entry point
+ * Return: Always 1
+ */
+
 int main(void){
-//	puts("\"Programming is like building a multilingual puzzle");
-//	printf("with proper grammar, but the outcome is a piece of art,\n")
-//	printf("Size of a char: %d byte(s)\n",sizeof(char));
-//	printf("Size of an int: %d byte(s)\n",sizeof(int));
-//	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
-//	printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
-//	printf("Size of a float: %d byte(s)\n",sizeof(float));
-write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59)
+        write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
