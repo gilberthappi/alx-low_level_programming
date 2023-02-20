a0ce24c1545584e445d54c2afde480e8d5204db2
Eng Happi Gilbert <gdushimimana6@gmail.com>
2023-02-20 02:23:13 -0800

    rwanda

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include<stdio.h>
 /**
 *main ~ Entry point
@@ -5,6 +6,6 @@
 Return always 0 (Success)
 */
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
 }
