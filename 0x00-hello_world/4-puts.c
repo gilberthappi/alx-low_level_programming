81498c40df1a594617f03ed4b0d9b4b050a9125a
Eng Happi Gilbert <gdushimimana6@gmail.com>
2023-02-20 02:55:04 -0800

    uut

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #!/bin/bash
 #include<stdio.h>
 /**
-*main ~ Entry point
-*
-Return always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts(""Programming is like building a multilingual puzzle\n");
-//	printf("with proper grammar, but the outcome is a piece of art,\n")
-//	printf("Size of a char: %d byte(s)\n",sizeof(char));
-//	printf("Size of an int: %d byte(s)\n",sizeof(int));
-//	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
-//	printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
-//	printf("Size of a float: %d byte(s)\n",sizeof(float));
-//write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59)
-	return (0);
+
+    return (0);
 }
+
+
